594f42c5a387c8ca4ff37aa3ed2be3f21c7357ff
Dsprog <dsprog@gmail.com>
2021-07-21 00:02:50 -0300

    primeira model de teste

--- config/routes.php
+++ config/routes.php
@@ -1,11 +1,19 @@
 <?php
 
+use App\Models\User;
+
 $router->add('GET', '/', function() use ($container) {
     var_dump($container['db']);
     return 'HOME';
 });
 
 $router->add('GET', '/contato', function(){
+    return 'Contato';
+});
+
+$router->add('GET', '/usuario/(\d+)', function($params) use ($container){
+    $user = (new User($container))->get($params[1]);
+    var_dump($user); exit;
     return 'Contato';
 });
 
