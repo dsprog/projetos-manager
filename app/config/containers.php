3f927b1d5bb24349b85c9590013ee3df216af3de
Daniel Henrique dos Santos Oliveira <dsprog@gmail.com>
2021-07-29 09:28:46 -0300

    criando migration e arquivos de helpes TOOLS

--- app/config/containers.php
+++ app/config/containers.php
@@ -1,23 +1,38 @@
 <?php
 
-$container['db'] = function() {
-    $dns = 'mysql:host=localhost;dbname=pp_project_manager';
-    $username = 'root';
-    $password = '';
-    $options = [
-        \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
-    ];
+$container['events'] = function() {
+    return new Laminas\EventManager\EventManager;
+};
 
-    $pdo = new \PDO($dns, $username, $password, $options);
+$container['settings'] = function (){
+    return [
+            'db' => [
+                'dsn' => 'mysql:host=localhost;',
+                'database' => 'pp_project_manager',
+                'username' => 'root',
+                'password' => '',
+                'options' => [
+                    \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+                ]
+            ]
+        ];
+};
+
+$container['db'] = function ($c){
+    $dsn = $c['settings']['db']['dsn'] . 'dbname=' . $c['settings']['db']['database'];
+    $username = $c['settings']['db']['username'];
+    $password = $c['settings']['db']['password'];
+    $options = $c['settings']['db']['options'];
+
+    $pdo = new \PDO($dsn, $username, $password, $options);
+
     $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
     return $pdo;
 };
 
-$container['events'] = function() {
-    return new Laminas\EventManager\EventManager;
-};
-
 $container['users_model'] = function($c) {
     return new \App\Models\Users($c);
 };
+
+return $container;
